93a7e82dd6af86bf5dc2b6e4930c242895868e6b
Servet93 <servetseker93@gmail.com>
2020-08-27 00:35:12 +0300

    istemciden sunucuya text gönderimi.Sunucunun istemciye kural dısı isteklerde uyari vermesi ve soketi kapatmasi tamamlandi.Flood gönderimi mevcut.

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ClientApp
 {
@@ -10,8 +11,143 @@ namespace ClientApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Konusarak Ogren Istemci Yazılımı Başlatıldı.");
-            Console.ReadLine();
+            Console.WriteLine("Konusarak Ogren Istemci Yazilimi Baslatildi.");
+            var client = new Client();
+
+            do
+            {
+                Console.WriteLine("Flood Atilsin Mi?.E girilirse atilacak.");
+                Console.Write("Text Giriniz : ");
+
+                var text = Console.ReadLine();
+                if (text.Equals("E"))
+                {
+                    client.Send("Flood 1");
+                    Thread.Sleep(250);
+                    client.Send("Flood 2");
+                    Thread.Sleep(250);
+                    client.Send("Flood 3");
+                    Thread.Sleep(250);
+                    client.Send("Flood 4");
+                }
+                else
+                    client.Send(text);
+
+            } while (true);
+
+        }
+    }
+
+    public class Client
+    {
+        private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        public Client(string ip = "127.0.0.1", int port = 34567)
+        {
+            serverSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+            serverSocket.BeginConnect(new IPEndPoint(IPAddress.Parse(ip), port), (ar) => {
+                serverSocket.EndConnect(ar);
+
+                SocketReceive();
+
+            }, null);
+        }
+
+        private void SocketReceive()
+        {
+            var stateObject = new StateObject();
+            serverSocket.BeginReceive(stateObject.Header, 0, stateObject.HeaderLength, SocketFlags.None, (ar) => {
+
+                var receivedByteLength = serverSocket.EndReceive(ar);
+
+                if (receivedByteLength == 0)
+                    return;
+
+                var bodyLength = BitConverter.ToInt32(stateObject.Header, 0);
+                Console.WriteLine($"receivedByteLength : { receivedByteLength } / bodyLength : { bodyLength }");
+
+                stateObject.BufferLength = bodyLength;
+                serverSocket.BeginReceive(stateObject.Buffer, 0, bodyLength, SocketFlags.None, (ar2) => {
+                    receivedByteLength = serverSocket.EndReceive(ar2);
+
+                    if (receivedByteLength == 0)
+                        return;
+
+                    var receivedText = Encoding.UTF32.GetString(stateObject.Buffer);
+                    Console.WriteLine($"receivedText from server : {receivedText}");
+                }, stateObject);
+
+
+                SocketReceive();
+
+            }, stateObject);
+        }
+
+        public void Send(string text)
+        {
+            try
+            {
+                if (!serverSocket.Connected) { 
+                    Console.WriteLine("Bağlantı Hatası");
+                    return;
+                }
+
+                var textBytes = Encoding.UTF32.GetBytes(text);
+                var LengthBytes = BitConverter.GetBytes(textBytes.Length);
+
+                serverSocket.BeginSend(LengthBytes, 0, LengthBytes.Length, SocketFlags.None, (ar) => {
+                    var sendedByte = 0;
+                    try
+                    {
+                        sendedByte = serverSocket.EndSend(ar);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Bağlantı Hatası:{e.Message}");
+                    }
+
+                    if (sendedByte == 0)
+                        return;
+
+                    if (!serverSocket.Connected)
+                    {
+                        Console.WriteLine("Bağlantı Hatası");
+                        return;
+                    }
+
+                    serverSocket.BeginSend(textBytes, 0, textBytes.Length, SocketFlags.None, (ar2) =>
+                    {
+                        try
+                        {
+                            sendedByte = serverSocket.EndSend(ar2);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Bağlantı Hatası:{e.Message}");
+                        }
+
+                    }, null);
+
+                }, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Send Exception : {ex.ToString()}");
+            }
+        }
+    }
+
+    public class StateObject
+    {
+        public int HeaderLength { get; set; } = 4;
+        public int BufferLength { set { Buffer = new byte[value]; } }
+        public byte[] Header { get; private set; }
+        public byte[] Buffer { get; private set; }
+
+        public StateObject()
+        {
+            Header = new byte[HeaderLength];
         }
     }
 }
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ServerApp
 {
@@ -14,6 +12,7 @@ namespace ServerApp
         {
             Console.WriteLine($"Konusarak Ogren Sunucu Yazılımı Başlatıldı.");
             var listener = new Listener(34567);
+            listener.IIncomingHandler = new IncomingHandler();
 
             Console.ReadLine();
         }
@@ -22,18 +21,21 @@ namespace ServerApp
     public class Listener
     {
         private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public IIncomingHandler IIncomingHandler { get; set; }
 
         public Listener(int port)
         {
             serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            serverSocket.Listen(10);
+            SocketAccept();
         }
 
         private void SocketAccept()
         {
             serverSocket.BeginAccept((ar) =>
             {
-                SocketAccept();
                 var clientSocket = serverSocket.EndAccept(ar);
+                SocketAccept();
 
                 SocketReceive(clientSocket, new StateObject(clientSocket));
 
@@ -45,10 +47,7 @@ namespace ServerApp
             if (!clientSocket.Connected)
                 return;
 
-            clientSocket.BeginReceive(stateObject.Buffer, 0, 256, SocketFlags.None, (ar) => {
-
-                var _stateObject = ar.AsyncState as StateObject;
-                var _clientSocket = _stateObject.ClientSocket;
+            clientSocket.BeginReceive(stateObject.Header, 0, stateObject.HeaderLength, SocketFlags.None, (ar) => {
 
                 if (!clientSocket.Connected)
                     return;
@@ -58,23 +57,146 @@ namespace ServerApp
                 if (receivedByteLength == 0)
                     return;
 
-                var receivedText = Encoding.UTF8.GetString(_stateObject.Buffer);
+                var bodyLength = BitConverter.ToInt32(stateObject.Header, 0);
+                Console.WriteLine($"receivedByteLength : { receivedByteLength } / bodyLength : { bodyLength }");
 
-                Console.WriteLine($"receivedText : {receivedText}");
+                stateObject.BufferLength = bodyLength;
+                clientSocket.BeginReceive(stateObject.Buffer, 0, bodyLength, SocketFlags.None, (ar2) => {
+
+                    var receivedByteLength2 = clientSocket.EndReceive(ar2);
+
+                    if (receivedByteLength2 == 0)
+                        return;
+
+                    var _stateObject = ar.AsyncState as StateObject;
+                    //var _clientSocket = _stateObject.ClientSocket;
+
+                    var receivedText = Encoding.UTF32.GetString(_stateObject.Buffer);
+                    Console.WriteLine($"receivedText : {receivedText}");
+                    IIncomingHandler.Handler(this, clientSocket, DateTime.Now, receivedText);
+                    SocketReceive(clientSocket, new StateObject(clientSocket));
+                }, stateObject);
 
             }, stateObject);
+        }
+
+        public void Send(Socket clientSocket, string text)
+        {
+            var textBytes = Encoding.UTF32.GetBytes(text);
+            var LengthBytes = BitConverter.GetBytes(textBytes.Length);
+
+            Console.WriteLine($"LengthBytes : {LengthBytes.Length}");
+
+            clientSocket.BeginSend(LengthBytes, 0, LengthBytes.Length, SocketFlags.None, (ar) => {
+                var sendedByte = 0;
+
+                try
+                {
+                    sendedByte = clientSocket.EndSend(ar);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+
+                if (sendedByte == 0)
+                    return;
+
+                clientSocket.BeginSend(textBytes, 0, textBytes.Length, SocketFlags.None, (ar2) =>
+                {
+                    try
+                    {
+                        sendedByte = clientSocket.EndSend(ar2);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    if (sendedByte == 0)
+                        return;
+                }, null);
+
+            }, null);
         }
     }
 
     public class StateObject
     {
+        public int HeaderLength { get; set; } = 4;
+        public int BufferLength { set { Buffer = new byte[value]; } }
         public Socket ClientSocket { get; private set; }
+        public byte[] Header { get; private set; }
         public byte[] Buffer { get; private set; }
 
         public StateObject(Socket clientSocket)
         {
             ClientSocket = clientSocket;
-            Buffer = new byte[256];
+            Header = new byte[HeaderLength];
         }
+    }
+
+    public interface IIncomingHandler
+    {
+        void Handler(Listener listener, Socket socket, DateTime receivedDateTime, string text);
+    }
+
+    public class IncomingHandler : IIncomingHandler
+    {
+        public IncomingHandler()
+        {
+        }
+
+        /// <summary>
+        /// key(string) : remoteEndPoint,
+        /// value(tuple<datetime,int,string) : receivedtime,received message count at one second,received text data
+        /// </summary>
+        Dictionary<string, PackageInformationOfSocket> socketToData = new Dictionary<string, PackageInformationOfSocket>();
+        public void Handler(Listener listener, Socket clientSocket, DateTime receivedDateTime, string text)
+        {
+            var remoteEndPoint = clientSocket.RemoteEndPoint.ToString();
+            Console.WriteLine($"socket.RemoteEndPoint.ToString():{remoteEndPoint}");
+
+            if (!socketToData.ContainsKey(remoteEndPoint))
+                socketToData.Add(remoteEndPoint, new PackageInformationOfSocket { 
+                    Text = text,
+                });
+
+            var socketData = socketToData[remoteEndPoint];
+
+            if (!socketData.LastReceivedDateTime.HasValue)
+                socketData.LastReceivedDateTime = receivedDateTime;
+            else { 
+                var diffReceived = receivedDateTime - socketData.LastReceivedDateTime.Value;
+                socketData.LastReceivedDateTime = receivedDateTime;
+
+                Console.WriteLine($"diffReceived.TotalSeconds:{diffReceived.TotalSeconds}");
+
+                if (diffReceived.TotalSeconds <= 1)
+                    socketData.ReceivedMessageCountAtOneSecond++;
+                else
+                    socketData.ReceivedMessageCountAtOneSecond = 0;
+
+                if (socketData.CountAttemptsOfSameError == 1)
+                    clientSocket.Close();
+                else
+                {
+                    if (socketData.ReceivedMessageCountAtOneSecond > 1)
+                    {
+                        listener.Send(clientSocket, "Flood tekrarladığınız taktirde bağlantınız koparılacaktır.");
+                        socketData.CountAttemptsOfSameError++;
+                    }
+                }
+            }
+        }
+    }
+
+    public class PackageInformationOfSocket
+    {
+        public DateTime? LastReceivedDateTime { get; set; }
+        public int ReceivedMessageCountAtOneSecond { get; set; } = 0;
+        public int CountAttemptsOfSameError { get; set; } = 0;
+        public string Text { get; set; }
     }
 }
